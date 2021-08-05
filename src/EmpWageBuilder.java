3e863e285d9faf411354cb347963981d3184bf84
SRC0747 <srcsrk1998@gmail.com>
2021-08-05 22:48:45 +0530

    added: Calculating employee wage for a month having 20 working days.

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -5,23 +5,26 @@ public class EmpWageBuilder {
         int IS_FULL_TIME=2;
         int IS_PART_TIME=1;
         int EMP_RATE_PER_HR=20;
+        int day=1;
         int empHrs=0;
         int empWage=0;
+        int workingDays=20;
         int empCheck=(int) Math.floor(Math.random()*10)%3;
-        switch (empCheck)
-        {
-            case 2:
-                System.out.println("Employee Full-time Present.");
-                empHrs=8;
-                break;
-            case 1:
-                System.out.println("Employee Part-time Present.");
-                empHrs=4;
-                break;
-            default:
-                System.out.println("Employee is absent.");
-                empHrs=0;
-                break;
+        for(day=1; day<=workingDays; day++) {
+            switch (empCheck) {
+                case 2:
+                    System.out.println("Employee Full-time Present.");
+                    empHrs = 8;
+                    break;
+                case 1:
+                    System.out.println("Employee Part-time Present.");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent.");
+                    empHrs = 0;
+                    break;
+            }
         }
         empWage=empHrs * EMP_RATE_PER_HR;
         System.out.println("Employee Wage is:"+empWage);
