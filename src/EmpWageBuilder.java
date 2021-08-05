9ad3487abfc8724541adb512c5a329192658c567
SRC0747 <srcsrk1998@gmail.com>
2021-08-05 21:36:09 +0530

    added: Checking Employee is Present or Absent.

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,6 +1,12 @@
 public class EmpWageBuilder {
     public static void main(String[] args)
     {
+        int IS_FULL_TIME=1;
         System.out.println("Welcome to Employee Wage computation program on Master Branch.");
+        int empCheck=(int) Math.floor(Math.random()*10)%2;
+        if(empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present.");
+        else
+            System.out.println("Employee is Absent.");
     }
 }
