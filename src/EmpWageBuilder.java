f5634b2a38c2ff58348716e7642e9048e5d9b71f
SRC0747 <srcsrk1998@gmail.com>
2021-08-05 22:36:08 +0530

    added: Calculating employee daily wage by using switch case.

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,24 +1,27 @@
 public class EmpWageBuilder {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage computation program on Master Branch.");
         int IS_FULL_TIME=2;
         int IS_PART_TIME=1;
         int EMP_RATE_PER_HR=20;
         int empHrs=0;
         int empWage=0;
-        System.out.println("Welcome to Employee Wage computation program on Master Branch.");
         int empCheck=(int) Math.floor(Math.random()*10)%3;
-        if(empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Full-time Present.");
-            empHrs = 8;
-        }
-        if(empCheck == IS_PART_TIME) {
-            System.out.println("Employee is Part-time Present.");
-            empHrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent.");
-            empHrs = 0;
+        switch (empCheck)
+        {
+            case 2:
+                System.out.println("Employee Full-time Present.");
+                empHrs=8;
+                break;
+            case 1:
+                System.out.println("Employee Part-time Present.");
+                empHrs=4;
+                break;
+            default:
+                System.out.println("Employee is absent.");
+                empHrs=0;
+                break;
         }
         empWage=empHrs * EMP_RATE_PER_HR;
         System.out.println("Employee Wage is:"+empWage);
