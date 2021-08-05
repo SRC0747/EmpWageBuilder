a6e65ea2a1ff944337f9f4640a05a005ad97a606
SRC0747 <srcsrk1998@gmail.com>
2021-08-05 22:03:23 +0530

    added: Calculating daily employee wage of full-time and part-time and absent employees.

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,15 +1,20 @@
 public class EmpWageBuilder {
     public static void main(String[] args)
     {
-        int IS_FULL_TIME=1;
+        int IS_FULL_TIME=2;
+        int IS_PART_TIME=1;
         int EMP_RATE_PER_HR=20;
         int empHrs=0;
         int empWage=0;
         System.out.println("Welcome to Employee Wage computation program on Master Branch.");
-        int empCheck=(int) Math.floor(Math.random()*10)%2;
+        int empCheck=(int) Math.floor(Math.random()*10)%3;
         if(empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present.");
+            System.out.println("Employee is Full-time Present.");
             empHrs = 8;
+        }
+        if(empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Part-time Present.");
+            empHrs = 4;
         }
         else {
             System.out.println("Employee is Absent.");
